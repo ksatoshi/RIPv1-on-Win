6073d38456b4fe2851a0acb21081287a3ebe48ff
Satoshi Kawahira <ksatoshi@mail.ksatoshi.dev>
2023-04-07 18:15:55 +0900

    ビルドが通るように改善

--- ripv1-on-windows/create-header.cpp
+++ ripv1-on-windows/create-header.cpp
@@ -1,3 +1,5 @@
+unsigned* reset_header(unsigned*, int);
+
 //RIP用のヘッダーを生成し配列として返す関数
 /*
 *0                           16                       31
@@ -8,23 +10,28 @@
 *+----------------------------------------------------+
 *                                                     63
 */
-unsigned __int32* generate_rip_header(int command, int version, int afi) {
-	unsigned __int32 head[2] = { 0 };
-	unsigned long int head1 = 0x0000;
-	unsigned long int head2 = 0x0000;
+unsigned* generate_rip_header(unsigned *header,int header_length,int command, int version, int afi) {
+	//ヘッダーを0でリセット
+	header = reset_header(header, header_length);
 
-	head1 = head1 | command;
-	head1 = head1 << 8;
-	head1 = head1 | version;
-	head1 = head1 << 16;
+	//header_length==2の場合のみRIPヘッダーを作成して返す
+	if (header_length == 2) {
+		unsigned long int head1 = 0x00000000;
+		unsigned long int head2 = 0x00000000;
 
-	head2 = head2 | afi;
-	head2 = head2 << 16;
+		head1 = head1 | command;
+		head1 = head1 << 8;
+		head1 = head1 | version;
+		head1 = head1 << 16;
 
-	head[0] = head1;
-	head[1] = head2;
+		head2 = head2 | afi;
+		head2 = head2 << 16;
 
-	return head;
+		*header = head1;
+		*(header + 1) = head2;
+	}
+
+	return header;
 }
 
 //RIP用のIPAddressを含むヘッダーを生成
@@ -40,10 +47,23 @@ unsigned __int32* generate_rip_header(int command, int version, int afi) {
 |metric(32bit)                                       |
 +----------------------------------------------------+
 */
-unsigned __int32* generate_rip_header_ip(unsigned __int32 ip_addr, unsigned __int32 metric) {
-	unsigned __int32 head[4] = { 0 };
-	head[0] = ip_addr;
-	head[3] = metric;
+unsigned* generate_rip_header_ip(unsigned* header, int header_length, unsigned __int32 ip_addr, unsigned __int32 metric) {
+	//ヘッダーをリセット
+	header = reset_header(header, header_length);
 
-	return head;
+	//header_length==4の場合のみIPヘッダーを作成
+	if (header_length == 4) {
+		*header = ip_addr;
+		*(header + 3) = metric;
+	}
+
+	return header;
+}
+
+//ヘッダーを0にリセットする関数
+unsigned* reset_header(unsigned* header, int length) {
+	for (int i = 0; i < length; i++) {
+		*(header + i) = 0x00000000;
+	}
+	return header;
 }
--- ripv1-on-windows/ripv1-on-windows.cpp
+++ ripv1-on-windows/ripv1-on-windows.cpp
@@ -1,20 +1,60 @@
-﻿// ripv1-on-windows.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<WinSock2.h>
+#include<stdio.h>
 
-#include <iostream>
+unsigned* generate_rip_header(unsigned*, int, int, int, int);
+unsigned* generate_rip_header_ip(unsigned*, int, unsigned __int32, unsigned __int32);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+/*
+* wireshark filter
+* (ip.src==192.168.11.17 && udp && ip.dst==192.168.11.1) || (ip.src==192.168.11.1 && udp && ip.dst==192.168.11.17)
+*/
+
+int main() {
+	WSAData wsaData;
+	SOCKET sock;
+
+	struct sockaddr_in addr;
+	BOOL yes = 1;
+
+	int command = 1;
+	int version = 1;
+	int afi = 2;
+	unsigned __int32 ip_addr = 0xc0a00b01;
+	unsigned __int32 metric = 0x00000010;
+
+	unsigned *header, *ip_header;
+	int header_length = 2, ip_header_length = 4;
+
+	header = (unsigned*)malloc(sizeof(unsigned) * header_length);
+	ip_header = (unsigned*)malloc(sizeof(unsigned) * ip_header_length);
+
+	header = generate_rip_header(header, header_length, command, version, afi);
+	ip_header = generate_rip_header_ip(ip_header, ip_header_length, ip_addr, metric);
+
+	
+	unsigned __int32 sendHead[6];
+
+	for (int i = 0; i < 6; i++) {
+		printf("%x\n", sendHead[i]);
+	}
+
+	/*WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(520);
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	//setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&yes, sizeof(yes));
+
+	int send_size = sendto(sock, (char*)sendHead, sizeof(sendHead), 0, (struct sockaddr*)&addr, sizeof(addr));
+	printf("���M����!!\n");
+	printf("%d\n", sizeof(sendHead));
+
+	closesocket(sock);
+	WSACleanup();*/
+
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
